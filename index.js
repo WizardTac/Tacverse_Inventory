77e68a7b2e6a8599fec08fc7748bfc6f43c9656b
Rico Hoppe <rico.hoppe@horneburger-blasorchester.de>
2023-04-17 10:49:19 +0200

    Outsourced commands

--- index.js
+++ index.js
@@ -9,41 +9,14 @@ const client = new Client({
         GatewayIntentBits.GuildIntegrations,
     ]
 })
+const checkItems = require('./commands/checkitems.js');
+const addItem = require('./commands/additem.js');
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}`);
-        const additem = {
-        name: 'additem',
-        description: 'Assign a role to a member',
-        options: [
-            {
-            name: 'member',
-            description: 'The member to assign the role to',
-            type: 6,
-            required: true,
-            },
-            {
-            name: 'item',
-            description: 'The item to assign to the member',
-            type: 8,
-            required: true,
-            },
-        ],
-        };
-        const checkitems = {
-            name: 'checkitems',
-            description: 'View Items of a member.',
-            options: [
-                {
-                name: 'member',
-                description: 'The member to assign the role to',
-                type: 6,
-                required: true,
-                },
-            ],
-        };
-    const data = await client.application.commands.create(additem);
-    const data2= await client.application.commands.create(checkitems);
+
+    const data = await client.application.commands.create(addItem);
+    const data2= await client.application.commands.create(checkItems);
     console.log(data, data2);
 
     client.user.setPresence({
@@ -55,6 +28,7 @@ client.on('ready', async () => {
 });
 
 client.on('interactionCreate', async interaction => {
+
     console.log('interaction created');
     if (!interaction.isCommand()) return;
 
@@ -65,90 +39,14 @@ client.on('interactionCreate', async interaction => {
     // -------------------------------------- ADDITEM ---------------------------------
 
     if (commandName === 'additem') {
-        const role = options.getRole('item');
-        const member = options.getMember('member');
-
-        if(!role && !member) {
-            return interaction.reply({
-                content: 'Please provide both an item and a member.',
-                ephemeral: true
-            })
-        }
-        if (!role) {
-            return interaction.reply({
-            content: 'Please provide an item.',
-            ephemeral: true
-            });
-        }
-        if (!member) {
-            return interaction.reply({
-            content: 'Please provide a member.',
-            ephemeral: true
-            });
-        }
-
-        try {
-            await member.roles.add(role);
-            await interaction.reply(`Role ${role.name} assigned to ${member.user.tag}.`);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({
-            content: 'There was an error while assigning the role.',
-            ephemeral: true
-            });
-        }
-    }
+        addItem.execute(interaction);
+    };
 
     // ----------------------------------- CHECKITEMS ---------------------------------
 
     if (commandName === 'checkitems') {
-        const member = options.getMember('member');
+        checkItems.execute(interaction);
+    };
+}),
 
-        //const carrotSwordIMG = new MessageAttachment(`img/carrotsword.png`);
-
-        if (!member) {
-            return interaction.reply({
-            content: 'Please provide a member.',
-            ephemeral: true
-            });
-        }
-
-        const memberRoles = member.roles.cache.map(role => role.name);
-
-        const hasAdminRole = memberRoles.includes('TAC-Team');
-        const hasASSRole = memberRoles.includes('A.S.S');
-        const hasGASSRole = memberRoles.includes('Gold A.S.S');
-        const hasCarrotSwordRole = memberRoles.includes('Carrot Sword');
-        const hasTACsWizardHatRole = memberRoles.includes('TAC`s Wizard Hat');
-        //  const hasItemRole = memberRoles.includes('role_name');
-
-        if(hasASSRole || hasGASSRole || hasCarrotSwordRole || hasTACsWizardHatRole) {
-            await interaction.reply({
-                content:
-                    `__**${member.user.tag}**'s Inventory__
-
-                    ${hasASSRole ? `**A.S.S** item  <:ASS:1096791523850457238>` : ''}
-                    ${hasGASSRole ? `**Golden A.S.S** item <:GoldASS:1096791048795193486>` : ''}
-                    ${hasCarrotSwordRole ? `**Carrot Sword** item <:CarrotSword:1096793344119685120>` : ''}
-                    ${hasTACsWizardHatRole ? `**TAC's Wizard Hat** item <:TACsWizardHat:1096793805170147409>` : ''}
-
-                    ${hasAdminRole ? '+ is an **Admin** of this server' : ''}
-                    `,
-                ephemeral: false,
-            });
-        } else {
-            await interaction.reply({
-                content:
-                    `__**${member.user.tag}**'s Inventory__
-
-                    Nooo! You don't have any Items.     :(
-
-                    ${hasAdminRole ? 'but you are an **Admin** of this server, so you`re still cool B)' : ''}
-                    `,
-                ephemeral: false,
-            });
-        }
-    }
-});
-
-client.login('MTA5NjM1NzgzMjkyMTcyNzA2Nw.Gmzh63.fvvAfsBIKMt1ly1yXEYoO0GRdxW2sszifj3VzU');
+client.login('token-goes-here')
